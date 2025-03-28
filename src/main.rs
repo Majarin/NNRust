cf40f798f5852c2f86734fe1278c4b6e59acf673
Olaf Bua <olafbua@gmail.com>
2025-03-28 12:54:21 +0100

    test

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use burn::{
     prelude::*,
 };
 
+use burn::backend::Wgpu;
+
 #[derive(Module, Debug)]
 pub struct Model<B: Backend> {
     conv1: Conv2d<B>,
@@ -18,6 +20,34 @@ pub struct Model<B: Backend> {
     activation: Relu,
 }
 
-fn main () {
-    
+#[derive(Config, Debug)]
+pub struct ModelConfig {
+    num_classes: usize,
+    hidden_size: usize,
+    #[config(default = "0.5")]
+    dropout: f64,
+}
+
+impl ModelConfig {
+    /// Returns the initialized model.
+    pub fn init<B: Backend>(&self, device: &B::Device) -> Model<B> {
+        Model {
+            conv1: Conv2dConfig::new([1, 8], [3, 3]).init(device),
+            conv2: Conv2dConfig::new([8, 16], [3, 3]).init(device),
+            pool: AdaptiveAvgPool2dConfig::new([8, 8]).init(),
+            activation: Relu::new(),
+            linear1: LinearConfig::new(16 * 8 * 8, self.hidden_size).init(device),
+            linear2: LinearConfig::new(self.hidden_size, self.num_classes).init(device),
+            dropout: DropoutConfig::new(self.dropout).init(),
+        }
+    }
+}
+
+fn main() {
+    type MyBackend = Wgpu<f32, i32>;
+
+    let device = Default::default();
+    let model = ModelConfig::new(10, 512).init::<MyBackend>(&device);
+
+    println!("{}", model);
 }
